0fb276c89730fb28d8d1db5a1c483f1b82ca7b8f
AL-FAHAMI TOIHIR <altoihir@gmail.com>
2022-05-29 01:21:25 +0100

    assurance

--- pages/assurance.php
+++ pages/assurance.php
@@ -1,53 +1,4 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-  <!-- basic -->
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <!-- mobile metas -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
-  <!-- site metas -->
-  <title>Site officiel ACEM</title>
-  <meta name="keywords" content="">
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <!-- site icons -->
-  <link rel="icon" href="images/fevicon/log_acem.png" type="image/gif" />
-  <!-- bootstrap css -->
-  <link rel="stylesheet" href="css/bootstrap.min.css" />
-  <!-- Site css -->
-  <link rel="stylesheet" href="css/style.css" />
-  <!-- responsive css -->
-  <link rel="stylesheet" href="css/responsive.css" />
-  <!-- colors css -->
-  <link rel="stylesheet" href="css/colors1.css" />
-  <!-- custom css -->
-  <link rel="stylesheet" href="css/custom.css" />
-  <!-- wow Animation css -->
-  <link rel="stylesheet" href="css/animate.css" />
-
-
-  <!-- revolution slider css -->
-  <link rel="stylesheet" type="text/css" href="revolution/css/settings.css" />
-  <link rel="stylesheet" type="text/css" href="revolution/css/layers.css" />
-  <link rel="stylesheet" type="text/css" href="revolution/css/navigation.css" />
-
-<!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    </head>
-    <body id="default_theme" class="it_service">
-      <!---<div class="bg_load"> <img class="loader_animation" src="images/fevicon/log_acem.png" /> </div>-->
-
-      <!-- header -->
-      <?php include('Header.php');  ?>
-      <!-- end header -->
+<?php require '../bootstrap.php'; ?>
       <!-- inner page banner -->
       <div id="inner_banner" class="section inner_banner_section">
         <div class="container">
@@ -76,7 +27,7 @@
             <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 pull-right">
               <div class="full">
                 <div class="blog_section">
-                  <div class="blog_feature_img"> <img class="img-responsive" src="images/it_service/wafa1.jpg" alt="#"> </div>
+                  <div class="blog_feature_img"> <img class="img-responsive" src="<?php echo URLROOT; ?>/assets/images/it_service/wafa1.jpg" alt="#"> </div>
                   <div class="blog_feature_cantant">
                     <p class="blog_head">Assurance AMCI</p>
                     <div class="post_info">
@@ -91,7 +42,7 @@
                       ainsi assure tous les étudiants étrangers au Maroc. L’assurance maladie concerne donc tous les étudiants
                     immatriculés à l’AMCI avec ou sans bourse. Pour les boursiers.... </p>
                     <div class="bottom_info">
-                      <div class="pull-left"><a class="btn sqaure_bt" href="ASSURANCE_MALADIE.pdf" target="_blank">En savoir plus<i class="fa fa-angle-right"></i></a></div>
+                      <div class="pull-left"><a class="btn sqaure_bt" href="<?php echo URLROOT; ?>/assets/docs/pdfs/ASSURANCE_MALADIE.pdf" target="_blank">En savoir plus<i class="fa fa-angle-right"></i></a></div>
                       <div class="pull-right">
                         <div class="shr">vidéo complète ici 👉 </div>
                         <div class="social_icon">
@@ -104,7 +55,7 @@
                   </div>
                 </div>
                 <div class="blog_section">
-                  <div class="blog_feature_img"> <img class="img-responsive" src="images/it_service/siege.jpg" alt="#"> </div>
+                  <div class="blog_feature_img"> <img class="img-responsive" src="<?php echo URLROOT; ?>/assets/images/it_service/siege.jpg" alt="#"> </div>
                   <div class="blog_feature_cantant">
                     <p class="blog_head">Bureau de Wafa-Assurance </p>
                     <div class="post_info">
@@ -116,14 +67,14 @@
                     </div>
                     <p>Vous pouvez telecharger directement  le document portant l'ensemble des bureaux Wafa-Assurance au maroc... </p>
                     <div class="bottom_info">
-                      <div class="pull-left"><a class="btn sqaure_bt" href="Bureau-wafa.xlsx" target="_blank">En savoir plus <i class="fa fa-angle-right"></i></a></div>
+                      <div class="pull-left"><a class="btn sqaure_bt" href="<?php echo URLROOT; ?>/assets/docs/excels/Bureau-wafa.xlsx" target="_blank">En savoir plus <i class="fa fa-angle-right"></i></a></div>
                     </div>
                   </div>
                 </div>
 
 
                 <div class="blog_section">
-                  <div class="blog_feature_img"> <img class="img-responsive" src="images/it_service/siege.jpg" alt="#"> </div>
+                  <div class="blog_feature_img"> <img class="img-responsive" src="<?php echo URLROOT; ?>/assets/images/it_service/siege.jpg" alt="#"> </div>
                   <div class="blog_feature_cantant">
                     <p class="blog_head">Flyer  Wafa-Assurance </p>
                     <div class="post_info">
@@ -135,7 +86,7 @@
                     </div>
                     <p>Pour en savoir plus sur une assistance medicale,information medicale,Assurance de sante,les contacts , consultez le flyer de Wafa-Assurance... </p>
                     <div class="bottom_info">
-                      <div class="pull-left"><a class="btn sqaure_bt" href="flyer.pdf" target="_blank">En savoir plus <i class="fa fa-angle-right"></i></a></div>
+                      <div class="pull-left"><a class="btn sqaure_bt" href="<?php echo URLROOT; ?>/assets/docs/pdfs/flyer.pdf" target="_blank">En savoir plus <i class="fa fa-angle-right"></i></a></div>
                     </div>
                   </div>
                 </div>
@@ -160,7 +111,7 @@
                       </li>
 
                       <li>
-                        <p class="post_head">Consultez les prestataires de santé conventionés <a href="Prestataires.xls" target="_blank"> cliquez ici.. </a></p>
+                        <p class="post_head">Consultez les prestataires de santé conventionés <a href="<?php echo URLROOT; ?>/assets/docs/excels/Prestataires.xls" target="_blank"> cliquez ici.. </a></p>
                         <p class="post_date"><i class="fa fa-calendar" aria-hidden="true"></i> 11 Avril, 2022</p>
                       </li>
                     </ul>
@@ -185,23 +136,4 @@
         </div>
       </div>
       <!-- end section -->
-
-
-
-      <!-- End Model search bar -->
-      <!-- footer -->
-      <?php include("footer.php") ?>
-
-      <!-- js section -->
-      <script src="js/jquery.min.js"></script>
-      <script src="js/bootstrap.min.js"></script>
-      <!-- menu js -->
-      <script src="js/menumaker.js"></script>
-      <!-- wow animation -->
-      <script src="js/wow.js"></script>
-      <!-- custom js -->
-      <script src="js/custom.js"></script>
-
-
-    </body>
-    </html>
+<?php require APPROOT . '/includes/footer.php'; ?>
