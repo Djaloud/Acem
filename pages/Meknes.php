b8903ac75f45df9e9f2df39a15457d7557685c4c
AL-FAHAMI TOIHIR <altoihir@gmail.com>
2022-05-29 02:04:49 +0100

    Meknes

--- pages/Meknes.php
+++ pages/Meknes.php
@@ -1,56 +1,5 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-  <!-- basic -->
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <!-- mobile metas -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
-  <!-- site metas -->
-  <title>Site officiel ACEM</title>
-  <meta name="keywords" content="">
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <!-- site icons -->
-  <link rel="icon" href="images/fevicon/log_acem.png" type="image/gif" />
-  <!-- bootstrap css -->
-  <link rel="stylesheet" href="css/bootstrap.min.css" />
-  <!-- Site css -->
-  <link rel="stylesheet" href="css/style.css" />
-  <!-- responsive css -->
-  <link rel="stylesheet" href="css/responsive.css" />
-  <!-- colors css -->
-  <link rel="stylesheet" href="css/colors1.css" />
-  <!-- custom css -->
-  <link rel="stylesheet" href="css/custom.css" />
-  <!-- wow Animation css -->
-  <link rel="stylesheet" href="css/animate.css" />
-
-
-  <!-- revolution slider css -->
-  <link rel="stylesheet" type="text/css" href="revolution/css/settings.css" />
-  <link rel="stylesheet" type="text/css" href="revolution/css/layers.css" />
-  <link rel="stylesheet" type="text/css" href="revolution/css/navigation.css" />
-
-<!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    </head>
-    <body id="default_theme" class="it_service">
-      <!---<div class="bg_load"> <img class="loader_animation" src="images/fevicon/log_acem.png" /> </div>-->
-      <!-- header -->
-      <?php include('Header.php');  ?>
-      <!-- end header -->
-
-      <!-- inner page banner -->
+<?php require '../bootstrap.php'; ?>
+<!-- inner page banner -->
       <div id="inner_banner" class="section inner_banner_section_meknes">
         <div class="container">
           <div class="row">
@@ -60,7 +9,7 @@
                   <div class="title-holder-cell text-left">
                     <h1 class="page-title">SECTION</h1>
                     <ol class="breadcrumb">
-                      <li><a href="index.html">Acceuil</a></li>
+                      <li><a href="<?php echo URLROOT; ?>">Acceuil</a></li>
                       <li class="active">Meknes</li>
                     </ol>
                   </div>
@@ -125,7 +74,7 @@
 
     <div class="col-md-4 col-sm-6">
       <div class="full team_blog_colum">
-        <div class="it_team_img"> <img class="img-responsive" src="photo/Meknes/Charfia_Mohamed.jpg" alt="#"> </div>
+        <div class="it_team_img"> <img class="img-responsive" src="<?php echo URLROOT; ?>/assets/images/photo/Meknes/Charfia_Mohamed.jpg" alt="#"> </div>
         <div class="team_feature_head">
           <h4>SECRETAIRE GENERAL<br>Charfia Mohamed Cheikh </h4>
           <button type="button" class="btn btn-primary"><a href="mailto:Charfiacheikh99@gmail.com">Contacter</a></button>
@@ -135,7 +84,7 @@
 
     <div class="col-md-4 col-sm-6">
       <div class="full team_blog_colum">
-        <div class="it_team_img"> <img class="img-responsive" src="photo/Meknes/Ali_Mfoihaya.jpeg" alt="#"> </div>
+        <div class="it_team_img"> <img class="img-responsive" src="<?php echo URLROOT; ?>/assets/images/photo/Meknes/Ali_Mfoihaya.jpeg" alt="#"> </div>
         <div class="team_feature_head">
           <h4>CHARGE SOCIO-CULTUREL<br>Ali Mfoihaya Djaoid </h4>
           <button type="button" class="btn btn-primary"><a href="mailto:alimfoihayadjaoid@gmail.com">Contacter</a></button>
@@ -145,7 +94,7 @@
 
     <div class="col-md-4 col-sm-6">
       <div class="full team_blog_colum">
-        <div class="it_team_img"> <img class="img-responsive" src="photo/Meknes/Saïd_rannia.jpeg" alt="#"> </div>
+        <div class="it_team_img"> <img class="img-responsive" src="<?php echo URLROOT; ?>/assets/images/photo/Meknes/Saïd_rannia.jpeg" alt="#"> </div>
         <div class="team_feature_head">
           <h4>CONTROLEUSE GENERALE<br>Saïd rannia  </h4>
           <button type="button" class="btn btn-primary"><a href="mailto:rannia140699@gmail.com">Contacter</a></button>
@@ -164,7 +113,7 @@
 
     <div class="col-md-4 col-sm-6">
       <div class="full team_blog_colum">
-        <div class="it_team_img"> <img class="img-responsive" src="photo/Meknes/Ali_kole.jpg" alt="#"> </div>
+        <div class="it_team_img"> <img class="img-responsive" src="<?php echo URLROOT; ?>/assets/images/photo/Meknes/Ali_kole.jpg" alt="#"> </div>
         <div class="team_feature_head">
           <h4>TRESORERIE GENERAL<br>Nawad Ali kole</h4>
           <button type="button" class="btn btn-primary"><a href="mailto:Saidnasur3459@gmail.com ">Contacter</a></button>
@@ -174,7 +123,7 @@
 
     <div class="col-md-4 col-sm-6">
       <div class="full team_blog_colum">
-        <div class="it_team_img"> <img class="img-responsive" src="photo/Meknes/Hatem_Mohamed.jpg" alt="#"> </div>
+        <div class="it_team_img"> <img class="img-responsive" src="<?php echo URLROOT; ?>/assets/images/photo/Meknes/Hatem_Mohamed.jpg" alt="#"> </div>
         <div class="team_feature_head">
           <h4>CHARGE SPORTIF ET COMMUNICATION<br>Hatem Mohamed Ali  </h4>
           <button type="button" class="btn btn-primary"><a href="mailto:hatemohamed01@gmail.com ">Contacter</a></button>
@@ -220,7 +169,7 @@
               <div class="carousel-item active">
                 <div class="testimonial-container">
                   <div class="testimonial-content"> <a href="https://www.bourses-etudiants.ma/bourse/bourse-detudes-germany-2021-bachelors-and-masters-degree-program-at-university-of-stuttgart/" style="color:#fff; font-size:" target="_blank">Bourse d’études Allemagne 2021: programme de licence et de master à l’Université de Stuttgart.</a> </div>
-                  <div class="testimonial-photo"> <img src="images/it_service/client1.jpg" class="img-responsive" alt="#" width="150" height="150"> </div>
+                  <div class="testimonial-photo"> <img src="<?php echo URLROOT; ?>/assets/images/it_service/client1.jpg" class="img-responsive" alt="#" width="150" height="150"> </div>
                   <div class="testimonial-meta">
                     <h4>7 janvier 2021</h4>
                     <span class="testimonial-position"><a href="https://www.bourses-etudiants.ma/bourse/bourse-detudes-germany-2021-bachelors-and-masters-degree-program-at-university-of-stuttgart/" style="color:#ffcccc;" target="_blank">En savoir plus.</a></span> </div>
@@ -230,7 +179,7 @@
                 <div class="carousel-item ">
                   <div class="testimonial-container">
                     <div class="testimonial-content"> <a href="https://www.bourses-etudiants.ma/bourse/bourse-de-recherche-zurich-2021-postdoctoral-program-at-swiss-federal-institute-of-technology-zurich/" style="color:#fff;" target="_blank">Bourse de Recherche Zurich 2021: Programme postdoctoral à l'Ecole polytechnique fédérale de Zurich.</a> </div>
-                    <div class="testimonial-photo"> <img src="images/it_service/bourse-suisse.png" class="img-responsive" alt="#" width="150" height="150"> </div>
+                    <div class="testimonial-photo"> <img src="<?php echo URLROOT; ?>/assets/images/it_service/bourse-suisse.png" class="img-responsive" alt="#" width="150" height="150"> </div>
                     <div class="testimonial-meta">
                       <h4>7 janvier 2021</h4>
                       <span class="testimonial-position"><a href="https://www.bourses-etudiants.ma/bourse/bourse-de-recherche-zurich-2021-postdoctoral-program-at-swiss-federal-institute-of-technology-zurich/" style="color:#ffcccc;" target="_blank">En savoir plus.</a></span> </div>
@@ -240,7 +189,7 @@
                   <div class="carousel-item ">
                     <div class="testimonial-container">
                       <div class="testimonial-content"> <a href="https://www.bourses-etudiants.ma/bourse/bourse-detudes-belgium-2021-masters-and-training-programmes-at-the-federation-wallonia-brussels/ " style="color:#fff;" target="_blank">Bourse d’études Belgique 2021: Masters et formations à la Fédération Wallonie-Bruxelles.</a> </div>
-                      <div class="testimonial-photo"> <img src="images/it_service/bourse-belgique.jpg" class="img-responsive" alt="#" width="150" height="150"> </div>
+                      <div class="testimonial-photo"> <img src="<?php echo URLROOT; ?>/assets/images/it_service/bourse-belgique.jpg" class="img-responsive" alt="#" width="150" height="150"> </div>
                       <div class="testimonial-meta">
                         <h4>7 janvier 2021</h4>
                         <span class="testimonial-position"><a href="https://www.bourses-etudiants.ma/bourse/bourse-detudes-belgium-2021-masters-and-training-programmes-at-the-federation-wallonia-brussels/ " style="color: #ffcccc;"target="_blank">En savoir plus.</a></span> </div>
@@ -259,37 +208,4 @@
         </div>
       </div>
       <!-- end section -->
-
-
-
-      <?php include("footer.php") ?>
-
-      <!-- end footer -->
-      <!-- js section -->
-      <script src="js/jquery.min.js"></script>
-      <script src="js/bootstrap.min.js"></script>
-      <!-- menu js -->
-      <script src="js/menumaker.js"></script>
-      <!-- wow animation -->
-      <script src="js/wow.js"></script>
-      <!-- custom js -->
-      <script src="js/custom.js"></script>
-      <!-- revolution js files -->
-      <script src="revolution/js/jquery.themepunch.tools.min.js"></script>
-      <script src="revolution/js/jquery.themepunch.revolution.min.js"></script>
-      <script src="revolution/js/extensions/revolution.extension.actions.min.js"></script>
-      <script src="revolution/js/extensions/revolution.extension.carousel.min.js"></script>
-      <script src="revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
-      <script src="revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
-      <script src="revolution/js/extensions/revolution.extension.migration.min.js"></script>
-      <script src="revolution/js/extensions/revolution.extension.navigation.min.js"></script>
-      <script src="revolution/js/extensions/revolution.extension.parallax.min.js"></script>
-      <script src="revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
-      <script src="revolution/js/extensions/revolution.extension.video.min.js"></script>
-      <!-- map js -->
-
-      <!-- google map js -->
-
-      <!-- end google map js -->
-    </body>
-    </html>
+<?php require APPROOT . '/includes/footer.php'; ?>
