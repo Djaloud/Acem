beba49e146d73d3f7d5e077ffd3506270c32335a
Djaloud Mohamed <mrdjaloud@gmail.com>
2022-06-16 00:15:56 +0100

    DB

--- pages/connectDB.php
+++ pages/connectDB.php
@@ -2,7 +2,7 @@
 function Connect(){
 
            try{
-                $conn = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME."",DB_USER,'');
+                $conn = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME."",DB_USER,DB_PASS);
                 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);     
             }
             
