259f66b7d2b2a6ea38de6dbdc29018c601ee78c6
Djaloud Mohamed <mrdjaloud@gmail.com>
2022-06-21 16:26:30 +0100

    DB

--- config/config.php
+++ config/config.php
@@ -3,10 +3,10 @@
 
 
 //configuration serveur 
-define('DB_HOST', 'db_host');
-define('DB_USER', 'db_user');
-define('DB_PASS', 'db_pass');
-define('DB_NAME', 'db_name');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'u213556546_acemOfficiel');
+define('DB_PASS', 'Bureau-executif2022');
+define('DB_NAME', 'u213556546_acem');
 
 
 //Configuration en local
