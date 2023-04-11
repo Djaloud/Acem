4213cdd0cf4a9de316dda4640768dc28adb2cad3
Sefdine <74672023+Sefdine@users.noreply.github.com>
2023-04-11 10:09:05 +0000

    Fix size of BE images at home

--- pages/sections/Casablanca.php
+++ pages/sections/Casablanca.php
@@ -155,7 +155,7 @@
 
         <div class="col-md-4 col-sm-6">
           <div class="full team_blog_colum">
-            <div class="it_team_img"> <img class="img-responsive" src="<?php echo URLROOT; ?>/assets/images/sections/casablanca/uf.png" alt="#"> </div>
+            <div class="it_team_img"> <img class="img-responsive" src="<?php echo URLROOT; ?>/assets/images/sections/casablanca/6.jpg" alt="#"> </div>
             <div class="team_feature_head">
               <h4>CHARGE DE LA COMMUNICATION<br>Balquis Mohamed</h4>
               <button type="button" class="btn btn-primary"><a href="mailto:">Contacter</a></button>
